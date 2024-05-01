032a5a8e67fe54a0bc59806f216273ac7d13a9d9
shiva <kuppashiva@gmail.com>
2024-05-01 04:13:42 -0400

    Updated Harpoon

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -69,17 +69,18 @@ keymap.set('n', '<leader>fm', function() require('telescope.builtin').treesitter
 keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>") -- toggle git blame
 
 -- Harpoon
-keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
-keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
-keymap.set("n", "<leader>h1", function() require("harpoon.ui").nav_file(1) end)
-keymap.set("n", "<leader>h2", function() require("harpoon.ui").nav_file(2) end)
-keymap.set("n", "<leader>h3", function() require("harpoon.ui").nav_file(3) end)
-keymap.set("n", "<leader>h4", function() require("harpoon.ui").nav_file(4) end)
-keymap.set("n", "<leader>h5", function() require("harpoon.ui").nav_file(5) end)
-keymap.set("n", "<leader>h6", function() require("harpoon.ui").nav_file(6) end)
-keymap.set("n", "<leader>h7", function() require("harpoon.ui").nav_file(7) end)
-keymap.set("n", "<leader>h8", function() require("harpoon.ui").nav_file(8) end)
-keymap.set("n", "<leader>h9", function() require("harpoon.ui").nav_file(9) end)
+local harpoon = require("harpoon")
+keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+keymap.set("n", "<leader>h1", function() harpoon:list().select(1) end)
+keymap.set("n", "<leader>h2", function() harpoon:list().select(2) end)
+keymap.set("n", "<leader>h3", function() harpoon:list().select(3) end)
+keymap.set("n", "<leader>h4", function() harpoon:list().select(4) end)
+keymap.set("n", "<leader>h5", function() harpoon:list().select(5) end)
+keymap.set("n", "<leader>h6", function() harpoon:list().select(6) end)
+keymap.set("n", "<leader>h7", function() harpoon:list().select(7) end)
+keymap.set("n", "<leader>h8", function() harpoon:list().select(8) end)
+keymap.set("n", "<leader>h9", function() harpoon:list().select(9) end)
 
 -- Vim REST Console
 keymap.set("n", "<leader>xr", ":call VrcQuery()<CR>") -- Run REST query
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,17 +1,26 @@
--- List of favorite files/marks per project
 return {
-  -- https://github.com/ThePrimeagen/harpoon
-  'ThePrimeagen/harpoon',
-  branch = 'master',
-  event = 'VeryLazy',
+  -- {{{ Define the Harpoon lazy.vim specificaiton.
+
+  "ThePrimeagen/harpoon",
+  enabled = true,
+  branch = "harpoon2",
   dependencies = {
     -- https://github.com/nvim-lua/plenary.nvim
     'nvim-lua/plenary.nvim',
+    -- https://github.com/nvim-telescope/telescope.nvim
+    'nvim-telescope/telescope.nvim',
   },
+
   opts = {
     menu = {
       width = 120
     }
   },
-}
 
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ Configure Harpoon.
+
+  config = function(_, opts)
+    require("harpoon").setup(opts)
+  end,
+}
